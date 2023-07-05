fe934d7340fbf7b665246c1420ce1fcc14050753
Roman Gafiatullin <rgafiatullin@appen.com>
2023-07-05 15:04:12 +0200

    Use verbum

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
 
+import Editor from './components/Editor/Editor.tsx'
+
 function App() {
   return (
-    <>
-      My App
-    </>
+    <Editor/>
   )
 }
 
---
+++ src/components/Editor/Editor.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react';
+
+import {
+  EditorComposer,
+  Editor as EditorComponent,
+  ToolbarPlugin,
+  AlignDropdown,
+  BackgroundColorPicker,
+  BoldButton,
+  CodeFormatButton,
+  FontFamilyDropdown,
+  FontSizeDropdown,
+  InsertDropdown,
+  InsertLinkButton,
+  ItalicButton,
+  TextColorPicker,
+  TextFormatDropdown,
+  UnderlineButton,
+  Divider,
+} from 'verbum';
+
+import './Editor.css';
+
+const Editor: FC = () => {
+  return (
+    <EditorComposer>
+      <EditorComponent hashtagsEnabled={true}>
+        <ToolbarPlugin defaultFontSize="20px">
+          <FontFamilyDropdown />
+          <FontSizeDropdown />
+          <Divider />
+          <BoldButton />
+          <ItalicButton />
+          <UnderlineButton />
+          <CodeFormatButton />
+          <InsertLinkButton />
+          <TextColorPicker />
+          <BackgroundColorPicker />
+          <TextFormatDropdown />
+          <Divider />
+          <InsertDropdown enablePoll={true} />
+          <Divider />
+          <AlignDropdown />
+        </ToolbarPlugin>
+      </EditorComponent>
+    </EditorComposer>
+  );
+};
+
+export default Editor;
